e9f2391dfb0bf021de7401f74fdcf67132324f60
Luidev <hackc7360@gmail.com>
2025-02-09 10:14:19 +0100

    fix bcrypt

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,13 +1,13 @@
-import { genSaltSync, hashSync, compareSync } from "bcrypt-ts"
+import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 
 export const hashPassword = async (password: string) => {
-  const salt = genSaltSync(10);
-  return hashSync(password, salt);
+  const salt = await bcrypt.genSalt(10);
+  return await bcrypt.hash(password, salt);
 };
 
 export const comparePassword = async (password: string, hash: string) => {
-  return compareSync(password, hash);
+  return await bcrypt.compare(password, hash);
 };
 
 export const generateJWT = (payload: object, secret: string) => {
